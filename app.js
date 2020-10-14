04555355706723b0cd23aed7143dde06ca70c4cc
stesha <stepashechka5@gmail.com>
2020-10-14 19:45:26 +0300

    First homework

---
+++ app.js
@@ -0,0 +1,11 @@
+let userNumber = 8;
+let result;
+
+result = 18 + userNumber;
+
+result = result - 6;
+result = result * 3;
+result = result / 12;
+
+alert('Полученный результат = ' + result + ';');
+alert('Неизмененная переменная, введенная пользователем = ' + userNumber + ';'); 
